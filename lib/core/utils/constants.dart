520ba5ac631bdf09785ff4281004cd4ce284a0fc
ramithks <ramithgowdakundoor123@gmail.com>
2024-08-05 17:04:14 +0530

    chore: Add new files and update dependencies

---
+++ lib/core/utils/constants.dart
@@ -0,0 +1,2 @@
+const String BASE_URL = 'https://api.openweathermap.org/data/2.5/';
+const String API_KEY = 'your_api_key';
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,16 @@
 import FlutterMacOS
 import Foundation
 
+import connectivity_plus
+import firebase_auth
+import firebase_core
+import geolocator_apple
+import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
