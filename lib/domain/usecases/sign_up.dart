dc1e83e310464cbaa4cbc3dc99aea7fabfc95bf5
ramithks <ramithgowdakundoor123@gmail.com>
2024-08-05 17:05:17 +0530

    chore: Remove unused sign_out and sign_up use cases

--- lib/domain/usecases/sign_up.dart
+++ lib/domain/usecases/sign_up.dart
@@ -1,12 +1,1 @@
-import '../entities/user.dart';
-import '../repositories/auth_repository.dart';
 
-class SignUp {
-  final AuthRepository repository;
-
-  SignUp(this.repository);
-
-  Future<User?> call(String email, String password) {
-    return repository.signUp(email, password);
-  }
-}
